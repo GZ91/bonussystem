eae9f4bceb69b385bd969e34a4fc34a41b3d008a
Георгий Зырянов <gzyryanov@icloud.com>
2023-07-01 19:39:10 +0300

    отменил предыдущий коммит, и исправил в модели данных структуру json отвечающую за ответ по ордерам

--- internal/models/models.go
+++ internal/models/models.go
@@ -17,7 +17,7 @@ type Claims struct {
 type DataOrder struct {
 	Number     string  `json:"number"`
 	Status     string  `json:"status"`
-	Accrual    float64 `json:"accural,omitempty"`
+	Accrual    float64 `json:"accrual,omitempty"`
 	UploadedAt string  `json:"uploaded_at"`
 }
 
